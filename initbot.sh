3acf625860d7909cf133a35ddb558ee9e340a612
enriquetecfan11 <enriquemondejar15@gmail.com>
2022-03-09 14:46:28 +0100

    fix sh file

--- initbot.sh
+++ initbot.sh
@@ -1,14 +1,15 @@
-echo("Hello, i build a bot for you")
-echo("Please run this script in the directory where you want to build the bot with the sudo comand")
-echo("                       ")
+echo "Hello, i build a bot for you"
+echo "Please run this script in the directory where you want to build the bot with the sudo comand"
 
-echo("Now we build the bot with the dockefile")
+echo "                       "
+
+echo "Now we build the bot with the dockefile"
 sudo docker build -t tecfanbot .
-echo("                       ")
-echo("CryptoBot build complete")
+echo "                       "
+echo "CryptoBot build complete"
 
 
-echo("Now we run the bot with the docker run command")
+echo "Now we run the bot with the docker run command"
 sudo docker run -it tecfanbot 
-echo("                       ")
-echo("CryptoBot it's running check telegram and write something") 
+echo "                       "
+echo "CryptoBot it's running check telegram and write something" 
