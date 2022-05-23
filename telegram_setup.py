953a2385c14f156dedb7befdd7616b32c6c2ef53
Enrique Rodriguez Vela <enriquemondejar15@gmail.com>
2022-05-23 11:30:55 +0000

    add new command

--- telegram_setup.py
+++ telegram_setup.py
@@ -1,17 +1,18 @@
-import os   
+from price_change import crypto_price
+from timenews import noticias_economicas
+from timenews import noticias
+from timenews import mondejar_weather
+from stonks_price import stonksPrice
+from datetime import datetime
+import os
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
 from dotenv import load_dotenv
 load_dotenv()
 
-from price_change import crypto_price
-from timenews import noticias
-from timenews import mondejar_weather
-from stonks_price import stonksPrice
-
-from datetime import datetime
 
 # Init the bot and the updater
+
 
 def init_bot():
     updater = Updater(os.getenv("TELEGRAM_TOKEN"), use_context=True)
@@ -21,25 +22,36 @@ def init_bot():
     # Define the commands
 
     def start(update: Update, context: CallbackContext):
-        context.bot.send_message(chat_id=update.message.chat_id, text="Hi! I'm a bot that will send you some things" + "\n" "I created by Enrique Rodriguez Vela." + "\n" +"If you want to see the comands type /help.")
+        context.bot.send_message(chat_id=update.message.chat_id, text="Hi! I'm a bot that will send you some things" +
+                                 "\n" "I created by Enrique Rodriguez Vela." + "\n" + "If you want to see the comands type /help.")
 
     def help(update: Update, context: CallbackContext):
-        context.bot.send_message(chat_id=update.message.chat_id, text="/help: Show this message" + "\n" + "/crypto: Show the current price of crypto currencies" + "\n" + "/news: Show the latest news" + "\n" + "/weather: Show the current weather in Mondejar" + "\n" + "/stonks: Show the current price of Stonks") 
+        context.bot.send_message(chat_id=update.message.chat_id, text="/help: Show this message" + "\n" + "/crypto: Show the current price of crypto currencies" +
+                                 "\n" + "/news: Show the latest news" + "\n" + "/weather: Show the current weather in Mondejar" + "\n" + "/stonks: Show the current price of Stonks")
 
     def stop(update: Update, context: CallbackContext):
-        context.bot.send_message(chat_id=update.message.chat_id, text="Goodbye!")
+        context.bot.send_message(
+            chat_id=update.message.chat_id, text="Goodbye!")
 
     def crypto(update: Update, context: CallbackContext):
-        context.bot.send_message(chat_id=update.message.chat_id, text=crypto_price())
+        context.bot.send_message(
+            chat_id=update.message.chat_id, text=crypto_price())
 
     def weather(update: Update, context: CallbackContext):
-        context.bot.send_message(chat_id=update.message.chat_id, text=mondejar_weather())
+        context.bot.send_message(
+            chat_id=update.message.chat_id, text=mondejar_weather())
 
     def news(update: Update, context: CallbackContext):
-        context.bot.send_message(chat_id=update.message.chat_id, text=noticias())
+        context.bot.send_message(
+            chat_id=update.message.chat_id, text=noticias())
 
     def stonks(update: Update, context: CallbackContext):
-        context.bot.send_message(chat_id=update.message.chat_id, text=stonksPrice())
+        context.bot.send_message(
+            chat_id=update.message.chat_id, text=stonksPrice())
+
+    def economicas(update: Update, context: CallbackContext):
+        context.bot.send_message(
+            chat_id=update.message.chat_id, text=noticias_economicas())
 
     # Check what the user is typedn and save it in the variable command
     def typed(update: Update, context: CallbackContext):
@@ -47,21 +59,28 @@ def init_bot():
         #print("User said: " + message)
 
         if message == "hola" or message == "hello" or message == "hi" or message == "Hola":
-            context.bot.send_message(chat_id=update.message.chat_id, text="Hola!")
+            context.bot.send_message(
+                chat_id=update.message.chat_id, text="Hola!")
         elif message == "adios" or message == "Adios" or message == "bye":
-            context.bot.send_message(chat_id=update.message.chat_id, text="Adios!")
+            context.bot.send_message(
+                chat_id=update.message.chat_id, text="Adios!")
         elif message == "que hora es":
-            context.bot.send_message(chat_id=update.message.chat_id, text="Son las " + datetime.now().strftime("%H:%M:%S"))
+            context.bot.send_message(
+                chat_id=update.message.chat_id, text="Son las " + datetime.now().strftime("%H:%M:%S"))
         elif message == "que dia es hoy":
-            context.bot.send_message(chat_id=update.message.chat_id, text="Es " + datetime.now().strftime("%A"))
+            context.bot.send_message(
+                chat_id=update.message.chat_id, text="Es " + datetime.now().strftime("%A"))
         elif message == "que dia es mañana":
-            context.bot.send_message(chat_id=update.message.chat_id, text="Es " + (datetime.now() + timedelta(days=1)).strftime("%A"))
+            context.bot.send_message(chat_id=update.message.chat_id, text="Es " + (
+                datetime.now() + timedelta(days=1)).strftime("%A"))
         elif message == "que puedes hacer":
-            context.bot.send_message(chat_id=update.message.chat_id, text="Puedes hacer lo siguiente: /help")
+            context.bot.send_message(
+                chat_id=update.message.chat_id, text="Puedes hacer lo siguiente: /help")
         elif message == "que es esto":
-            context.bot.send_message(chat_id=update.message.chat_id, text="Esto es un bot hecho por Enrique Rodriguez Vela")
-        
-        # If user post a message 
+            context.bot.send_message(
+                chat_id=update.message.chat_id, text="Esto es un bot hecho por Enrique Rodriguez Vela")
+
+        # If user post a message
 
     # Add the commands to the dispatcher
     dispatcher.add_handler(CommandHandler('start', start))
@@ -71,6 +90,7 @@ def init_bot():
     dispatcher.add_handler(CommandHandler('weather', weather))
     dispatcher.add_handler(CommandHandler('news', news))
     dispatcher.add_handler(CommandHandler('stonks', stonks))
+    dispatcher.add_handler(CommandHandler('economicas', economicas))
     dispatcher.add_handler(MessageHandler(Filters.text, typed))
 
     # Start the bot
@@ -78,4 +98,3 @@ def init_bot():
 
     # Run the bot
     updater.idle()
-        
--- timenews.py
+++ timenews.py
@@ -1,11 +1,12 @@
-from bs4 import BeautifulSoup 
+from bs4 import BeautifulSoup
 import requests
 from datetime import datetime
+
 
 def mondejar_weather():
     r = requests.get('https://www.wunderground.com/weather/es/mond%C3%A9jar/')
     soup = BeautifulSoup(r.text, 'lxml')
-    
+
     tiempo = soup.find_all('span', class_='wu-value wu-value-to')
     texto_tiempo = int(tiempo[0].text)
     celsius = (texto_tiempo - 32) * 5/9
@@ -14,9 +15,11 @@ def mondejar_weather():
     condiciones = soup.find_all('div', class_='condition-icon')
     condiciones_texto = condiciones[0].text
 
-    output = ("Mondejar have now " + str(redondeo) + "ºC and have " + condiciones_texto)
+    output = ("Mondejar have now " + str(redondeo) +
+              "ºC and have " + condiciones_texto)
     print("Weather Response send!")
     return output
+
 
 def noticias():
     r = requests.get('https://www.elespanol.com/')
@@ -33,3 +36,20 @@ def noticias():
     respone = first.text + second.text
     #print("News Response send!")
     return respone
+
+
+def noticias_economicas():
+    r = requests.get('https://www.elespanol.com/invertia/')
+    soup = BeautifulSoup(r.text, 'lxml')
+    news = soup.find_all('h3', class_="head head--l")
+
+    newslist = []
+    for new in news:
+        newslist.append(new)
+
+
+    first = newslist[0]
+    second = newslist[1]
+
+    respuesta = first.text + second.text
+    return respuesta
